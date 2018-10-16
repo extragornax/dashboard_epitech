be3ba0254c8c98407e91e4027e52468dd33a2393
Extragornax <gaspardwi@yahoo.fr>
2018-10-16 09:49:05 +0200

    update on all files

--- Dashboard_dotnet/Dashboard/Startup.cs
+++ Dashboard_dotnet/Dashboard/Startup.cs
@@ -25,21 +25,17 @@ namespace Dashboard
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddRouting();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             if (env.IsDevelopment())
-            {
                 app.UseDeveloperExceptionPage();
-            }
             else
-            {
                 app.UseHsts();
-            }
-
+        
             app.UseHttpsRedirection();
             app.UseMvc();
         }
